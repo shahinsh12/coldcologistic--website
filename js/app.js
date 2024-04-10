d829282e1fad0ed184dbdeb79dd7f9832fc4d041
MOHAMMAD SHAHINUL HAQUE <125897285+shahinsh12@users.noreply.github.com>
2024-04-10 23:52:54 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+//SJSVBMNLOVE
+$('.banner_slider').slick({
+  prevArrow: false,
+ nextArrow: false,
+  dots:true,
+  dotsClass:'banner_slider_dots',
+  fade: true,
+  autoplay: true,
+  autoplaySpeed:1400,
+ speed:1400,
+});
+$('.services_banner_slide').slick({
+  prevArrow: false,
+ nextArrow: false,
+  dots:true,
+  dotsClass:'service_banner_dots',
+  fade: true,
+  autoplay: true,
+  autoplaySpeed:1400,
+ speed:1400,
+});
+
